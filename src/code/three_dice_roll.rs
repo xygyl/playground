31f0039f1120270a78dd496a6e241c8da92f5f83
xygyl <xygyl@proton.me>
2025-04-23 15:20:28 -0500

    three_dice_roll() now takes in a u32

--- src/code/three_dice_roll.rs
+++ src/code/three_dice_roll.rs
@@ -5,7 +5,7 @@ use text_io::read;
 pub fn three_dice_roll() {
     print!("\nPlease input max range: ");
 
-    let i: u8 = read!();
+    let i: u32 = read!();
 
     loop {
         let r1 = rand::rng().random_range(0..=i);
@@ -13,7 +13,7 @@ pub fn three_dice_roll() {
         let r3 = rand::rng().random_range(0..=i);
 
         if r1 == r2 && r2 == r3 {
-            println!("{}, {}, {}", r1, r2, r3);
+            println!("{} {} {}", r1, r2, r3);
             break;
         }
     }
