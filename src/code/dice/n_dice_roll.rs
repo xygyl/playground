9798be29a1b334de48455519a2ff3e831780f612
xygyl <xygyl@proton.me>
2025-10-09 13:27:44 -0500

    rewrite n_dice_roll

--- src/code/dice/n_dice_roll.rs
+++ src/code/dice/n_dice_roll.rs
@@ -11,10 +11,14 @@ pub fn n_dice_roll() -> Option<()> {
             let roll_n: u32 = CustomType::new("Enter number of rolls:").prompt().ok()?;
             let rolls: String = (1..=roll_n)
                 .into_par_iter()
-                .map(|_| rand::rng().random_range(1..=sides))
-                .map(|v| format!("{}", v))
-                .collect::<Vec<_>>()
-                .join(" ");
+                .map(|_| rand::rng().random_range(1..=sides).to_string())
+                .reduce(|| String::new(), |mut a, b| {
+                    if !a.is_empty() {
+                        a.push(' ');
+                    }
+                    a.push_str(&b);
+                    a
+                });
             println!("{}", rolls);
             Some(())
         }
