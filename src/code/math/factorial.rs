4a2b3099c18066bf1d1b6b508e9600969e5d49e1
xygyl <xygyl@proton.me>
2025-11-17 13:14:26 -0600

    faster and cleaner factorial code

--- src/code/math/factorial.rs
+++ src/code/math/factorial.rs
@@ -1,8 +1,6 @@
 use inquire::CustomType;
 use num_bigint::BigUint;
-// use num_traits::One;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::ops::Mul;
 
 pub fn factorial() -> Option<()> {
     let num: u32 = CustomType::new("Enter n:")
@@ -12,11 +10,12 @@ pub fn factorial() -> Option<()> {
 
     // let result: BigUint = (1..=num).fold(BigUint::one(), |acc, x| acc * x);
     // let result: BigUint = (1..=num).map(BigUint::from).fold(BigUint::one(), Mul::mul);
-    let result: BigUint = (1..=num)
-        .into_par_iter()
-        .map(BigUint::from)
-        .reduce_with(Mul::mul)
-        .unwrap();
+    // let result: BigUint = (1..=num)
+    //     .into_par_iter()
+    //     .map(BigUint::from)
+    //     .reduce_with(Mul::mul)
+    //     .unwrap();
+    let result: BigUint = (1..=num).into_par_iter().map(BigUint::from).product();
     println!("{}! = {}", num, result);
     Some(())
 }
